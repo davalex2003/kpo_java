81485c191ac49c9c822f98d5e0c119c4abd3a0be
davalex2003 <97729979+davalex2003@users.noreply.github.com>
2022-11-13 23:56:50 +0300

    Add files via upload

---
+++ minidz_2/StudentMark.java
@@ -0,0 +1,11 @@
+public class StudentMark {
+    int id;
+    String name;
+    int mark = -1;
+    int question = 0;
+    boolean alreadyAnswered = false;
+    public StudentMark(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
